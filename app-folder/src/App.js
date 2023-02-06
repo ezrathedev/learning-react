d1f53d85901165f988b4d40256b41002865464c9
Ezra Thomas Hampton III <ezrathomashamptoniii@Quinyattas-MacBook-Pro.local>
2023-02-06 12:36:07 -0500

    working out the react bootstrap issue

--- app-folder/src/App.js
+++ app-folder/src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import HomePage from './pages/HomePage.js';
 import AboutPage from './pages/AboutPage';
 import Projects from './pages/Project';
+import BrandExample from './pages/nav';
 
 function App() {
   return (
+
+
     <div className="App">
+      <BrandExample />
       <h1>My Awesome Blog</h1>
       <div id="page-body">
       <HomePage />
